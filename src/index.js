16dc62e496ba9028c9a7268ca56918760e178935
ara-97 <aravind.m@acko.com>
2019-11-20 18:05:23 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const url = require('url');
+const {errorResponse} = require("./response");
+const {original, resize} = require("./image");
+
+exports.handler = (event) => new Promise((resolve, reject) => {
+    const imageBucket = process.env.BUCKET;
+
+    if (!imageBucket) {
+        return reject(`Error: Set environment variable BUCKET`);
+    }
+
+    const path = event.path;
+    const objectKey = url.parse(path).pathname.replace(/^\/+/g, '');
+    console.log('INFO: key: ' + objectKey);
+
+    const queryParameters = event.queryStringParameters || {};
+
+    if (!queryParameters.width && !queryParameters.height) {
+        return original(imageBucket, objectKey)
+            .then(resolve)
+            .catch(reject);
+    }
+
+    const width = parseInt(queryParameters.width);
+    const height = parseInt(queryParameters.height);
+
+    if ((queryParameters.width && isNaN(width)) || (queryParameters.height && isNaN(height))) {
+        return reject(errorResponse(`width and height parameters must be integer`, 400));
+    }
+
+    return resize(imageBucket, objectKey, width, height)
+        .then(resolve)
+        .catch(reject);
+});
